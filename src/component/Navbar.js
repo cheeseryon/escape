86c23d10e866441e14d850f3aaaab97ee39aedf0
merona <ldotoroo@naver.com>
2023-10-24 14:53:20 +0900

    navbar 수정

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../App.css'
 import { useNavigate } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import likeIcon from '../img/like.png'
 import { useSelector } from 'react-redux';
 
@@ -28,28 +28,17 @@ const Navbar = () => {
       <div className='head'>
         <div className="inner">
           <div className="logo" onClick={goHome} style={{cursor:'pointer'}}>또!방탈출</div>
-
-          <div className="gnb">
-            <ul className="gnbList">          
-              <li onClick={goHome}>테마정보</li>
-              <li>매장정보</li>
-            </ul>
-          </div>   
-
-          <div className="util">
-            <div className='searchArea'>
-              <input type="text" className="searchBox" placeholder="테마 검색" onKeyDown={(e)=> search(e)}/>
-              <FontAwesomeIcon icon={faSearch} className='searchIcon'/>
+            <div className="util">
+              <div className='searchArea'>
+                <input type="text" className="searchBox" placeholder="테마 검색" onKeyDown={(e)=> search(e)}/>
+                <FontAwesomeIcon icon={faSearch} className='searchIcon'/>
+              </div>
+              <div className="likeListPage"  onClick={goToMyPage}>
+                <img className='likeIcon' src={likeIcon}></img>
+                <span className='iconBg'></span>
+                <span>{likeId.length}</span>
+              </div>
             </div>
-            <div className="likeListPage"  onClick={goToMyPage}>
-              <img className='likeIcon' src={likeIcon}></img>
-              <span className='iconBg'></span>
-              <span>{likeId.length}</span>
-            </div>
-            <div className="myPage">
-              <FontAwesomeIcon icon={faUser} className='userIcon' onClick={goToMyPage}/>
-            </div>
-          </div>
           </div>
         </div>
     </>
